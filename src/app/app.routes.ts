1c2407cdd3939e12f83efd52d382a76abb36d733
ahamad236 <ahmadshaik4u@gmail.com>
2025-05-07 20:58:11 +0530

    Added all clientside services models and added components into components folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {
