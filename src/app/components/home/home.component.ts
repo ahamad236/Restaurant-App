019387824b8596f612bfdd9ffb10e5ef1597afaa
Ahamad <ahmadshaik4u@gmail.com>
2025-05-31 22:14:29 +0530

    Update home.component.ts
    Added menu list in homescreen

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MainHeaderComponent } from '../main-header/main-header.component';
 import { SidenavComponent } from '../sidenav/sidenav.component';
 import { MenuComponent } from '../menu/menu.component';
+import { MenuList } from '../../models/menuList';
 
 @Component({
   selector: 'app-home',
@@ -11,4 +12,5 @@ import { MenuComponent } from '../menu/menu.component';
 })
 export class HomeComponent {
 
+  menuList = signal<MenuList>();
 }
